0dd77ea45ca5395bc880383b93eaeac71638bb3f
Wilson Sung <wilsonsung@google.com>
2020-03-09 17:12:05 +0800

    Roll back performace files
    File list:
    kernel/sched/
    include/linux/sched/
    
    Bug: 150756696
    Bug: 149660093
    
    Change-Id: Ia90f5123ef3ab0d3322e974d174b915e80ed642e
    Signed-off-by: Wilson Sung <wilsonsung@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9394,7 +9394,6 @@ static void detach_task(struct task_struct *p, struct lb_env *env)
 
 	p->on_rq = TASK_ON_RQ_MIGRATING;
 	deactivate_task(env->src_rq, p, DEQUEUE_NOCLOCK);
-	lockdep_off();
 #ifdef CONFIG_SCHED_WALT
 	double_lock_balance(env->src_rq, env->dst_rq);
 	if (!(env->src_rq->clock_update_flags & RQCF_UPDATED))
