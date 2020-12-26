3d0f7f2d3908adef54067ced2878413121650247
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-25 21:39:23 -0800

    treewide: Suppress overly verbose log spam
    This tames quite a bit of the log spam and makes dmesg readable.
    
    Uses work from Danny Lin <danny@kdrag0n.dev>.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -798,9 +798,8 @@ static int __gic_populate_rdist(struct redist_region *region, void __iomem *ptr)
 		u64 offset = ptr - region->redist_base;
 		gic_data_rdist_rd_base() = ptr;
 		gic_data_rdist()->phys_base = region->phys_base + offset;
-#ifndef CONFIG_PRODUCT_REALME_TRINKET
-		//Nanwei.Deng@BSP.power.Basic 2018/05/01
-		pr_info("CPU%d: found redistributor %lx region %d:%pa\n",
+
+		pr_debug("CPU%d: found redistributor %lx region %d:%pa\n",
 			smp_processor_id(), mpidr,
 			(int)(region - gic_data.redist_regions),
 			&gic_data_rdist()->phys_base);
