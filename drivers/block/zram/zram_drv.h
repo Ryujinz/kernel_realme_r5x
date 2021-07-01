96a6044d50a2caf5ba8f8061103537ba3035b154
Yue Hu <huyue2@yulong.com>
2021-06-30 18:53:07 -0700

    UPSTREAM: zram: move backing_dev under macro CONFIG_ZRAM_WRITEBACK
    backing_dev is never used when not enable CONFIG_ZRAM_WRITEBACK and it's
    introduced from writeback feature.  So it's needless also affect
    readability in that case.
    
    Link: https://lkml.kernel.org/r/20210521060544.2385-1-zbestahu@gmail.com
    Signed-off-by: Yue Hu <huyue2@yulong.com>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Sergey Senozhatsky <senozhatsky@chromium.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Change-Id: I02fc667e3c07ebeccecc6b54cc9a928f9d733ed6
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -117,7 +117,6 @@ struct zram {
 	 * zram is claimed so open request will be failed
 	 */
 	bool claim; /* Protected by bdev->bd_mutex */
-	struct file *backing_dev;
 #ifdef CONFIG_ZRAM_WRITEBACK
 	struct file *backing_dev;
 	spinlock_t wb_limit_lock;
