096e3b11d14e7f41824f62245757c073792e9dba
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-07-31 11:04:52 +0100

    drm_atomic: Boost DDR bus regardless sched_boost * Covers libperfmgr implementation

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2249,7 +2249,10 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY))
+	/* Boost CPU and DDR when committing a new frame if sched_boost > 0 */
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) ||
+			sysctl_sched_boost) {
+		cpu_input_boost_kick();
 		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 
 	drm_modeset_acquire_init(&ctx, 0);
