35f4e7c2dd35a2fe2c3c39de6fef7c9b86314e95
Ryujinz <renaldigunawan34@gmail.com>
2021-08-21 13:03:41 +0700

    Revert "sysctl: promote sched_migration_cost_ns out of CONFIG_SCHED_DEBUG"
    This reverts commit 04bde34479b243f65d76dc7817666130955799d8.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -385,14 +385,7 @@ static struct ctl_table kern_table[] = {
 		.proc_handler   = proc_dointvec,
 	},
 #endif
-        {
-                .procname       = "sched_migration_cost_ns",
-                .data           = &sysctl_sched_migration_cost,
-                .maxlen         = sizeof(unsigned int),
-                .mode           = 0644,
-                .proc_handler   = proc_dointvec,
-        },
-#ifdef CONFIG_SCHED_DEBUG
+#ifdef CONFIG_SCHED_WALT
 	{
 		.procname       = "sched_cpu_high_irqload",
 		.data           = &sysctl_sched_cpu_high_irqload,
@@ -606,6 +599,13 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= sched_proc_update_handler,
 		.extra1		= &min_sched_tunable_scaling,
 		.extra2		= &max_sched_tunable_scaling,
+	},
+	{
+		.procname	= "sched_migration_cost_ns",
+		.data		= &sysctl_sched_migration_cost,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
 	},
 	{
 		.procname	= "sched_nr_migrate",
