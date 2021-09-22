f3bda3383474352933c8f53fb100d235977092be
Ryujinz <renaldigunawan34@gmail.com>
2021-09-22 15:56:54 +0700

    Kernel/fork.c: Increase Boost zygote forks
    Signed-off-by: Ryujinz <renaldigunawan34@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2104,10 +2104,10 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost CPU to the max for 50 ms when userspace launches an app */
+	/* Boost CPU to the max for 150 ms when userspace launches an app */
 	if (is_zygote_pid(current->pid)) {
-		cpu_input_boost_kick_max(50);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+		cpu_input_boost_kick_max(150);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 150);
 	}
 
 	/*
