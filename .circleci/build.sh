4ab5cbbfe99bbbf6d47f02ffc37ae900eb5c53d4
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-10-06 22:11:18 +0700

    localscript: Use LLVM=1 instead of define default tools to Clang/LLVM
    At commit c4983d3bfe75 already added support for switching
    to Clang or LLVM tools if using LLVM=1, so in this script
    there is no need to define any of that and just define LLVM=1
    in the Clang compiler.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -47,7 +47,8 @@ function compile() {
   make -j$(nproc) ARCH=arm64 O=out \
 	CC=${CLANG_ROOTDIR}/bin/clang \
 	CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
-	CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
+	CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi- \
+    LLVM=1
 
    if ! [ -a "$IMAGE" ]; then
 	finerr
